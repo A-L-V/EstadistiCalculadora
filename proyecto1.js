8860ea77d833244b539061e343ce82f277a436c4
ALV <richardalvaradoflores@gmail.com>
2020-06-20 22:31:26 -0500

    table
    Aumento de la creacion de la tabla

--- proyecto1.js
+++ proyecto1.js
@@ -4,16 +4,43 @@ function agrupados(){
      var table = document.getElementById("table");
      var intervalos = document.getElementsByClassName("interval");
      intervalos[0].addEventListener("click",function(){
-          var tr = document.createElement("tr");
-          tr.style.backgroundColor = "red"
-          table.appendChild(tr)
-          console.log(tr)
-          tr.innerHTML = "estoy aqui"
+          crear();
+          
+          
+          
      })
      intervalos[1].addEventListener("click",function(){
-          for(let i = 1; i < 3;i++){
-               var borrar = table.lastChild;
-               table.removeChild(borrar);
-          }
+          
+          var borrar = table.lastChild;
+          table.removeChild(borrar);
+          
      })
+}
+function crear(){
+     var tr = document.createElement("tr");
+     tr.setAttribute("class","fila");
+     table.appendChild(tr);
+     //creacion de una td
+     var td = document.createElement("td");
+     let i = 1
+     while(i < 3){
+          tr.appendChild(td); 
+          i++;
+          if(i = 1){
+               var input = document.createElement("input")
+               input.setAttribute("type","text")
+               input.setAttribute("class","columna1")
+               td.appendChild(input)
+          }
+          if( i = 2){
+               var texto = document.createElement(input)
+               texto.setAttribute("type","class")
+               texto.setAttribute("class","columna2")
+               td.appendChild(texto)
+          }
+
+     }
+     
+     
+          
 }
