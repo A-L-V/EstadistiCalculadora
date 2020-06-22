331f602027cabdc20258adf38454e1ff94080229
Richard Josue Alvarado Flores <richardalvaradoflores@gmail.com>
2020-06-22 16:31:46 -0500

    Tabla
    Mejora visual de la tabla para  datos agrupados

--- proyecto1.js
+++ proyecto1.js
@@ -1,46 +1,45 @@
 function agrupados(){
      document.getElementById("divAgrupados").style.display="block";
      document.getElementById("divNoAgrupados").style.display="none";
+
      var table = document.getElementById("table");
      var intervalos = document.getElementsByClassName("interval");
+     var creados = 0
+     
      intervalos[0].addEventListener("click",function(){
           crear();
-          
-          
-          
+          creados++;
      })
      intervalos[1].addEventListener("click",function(){
-          
-          var borrar = table.lastChild;
-          table.removeChild(borrar);
-          
+          if(creados > 0){
+               var borrar = table.lastChild;
+               table.removeChild(borrar);
+               creados--;
+          }
+
      })
 }
+
+
+
 function crear(){
      var tr = document.createElement("tr");
      tr.setAttribute("class","fila");
      table.appendChild(tr);
-     //creacion de una td
-     var td = document.createElement("td");
-     let i = 1
-     while(i < 3){
-          tr.appendChild(td); 
-          i++;
-          if(i = 1){
-               var input = document.createElement("input")
-               input.setAttribute("type","text")
-               input.setAttribute("class","columna1")
-               td.appendChild(input)
+     for( let i = 0; i<3;i++){
+          var td = document.createElement("td")
+          tr.appendChild(td)
+          if ( i== 0){
+               var text = document.createElement("input")
+               text.setAttribute("type","text")
+               text.setAttribute("class","columna1")
+               td.appendChild(text)
           }
-          if( i = 2){
-               var texto = document.createElement(input)
-               texto.setAttribute("type","class")
-               texto.setAttribute("class","columna2")
-               td.appendChild(texto)
+          if ( i == 1){
+               var number = document.createElement("input")
+               number.setAttribute("type","number")
+               text.setAttribute("type","columna2")
+               td.appendChild(number)
           }
-
-     }
-     
-     
-          
+     }    
 }
