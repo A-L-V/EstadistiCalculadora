589ca01ebcd01301966cae1773b7ebbd5e925f3f
ALV <richardalvaradoflores@gmail.com>
2020-06-22 23:26:25 -0500

    Horror0-2
    Se usa el metodo de las classes para la creacion de las casillas

--- proyecto1.js
+++ proyecto1.js
@@ -1,13 +1,34 @@
 function agrupados(){
      document.getElementById("divAgrupados").style.display="block";
      document.getElementById("divNoAgrupados").style.display="none";
-
+     var td = document.getElementsByClassName("td")
+     console.log(td)
+     var tr = document.getElementsByClassName("fila")
      var table = document.getElementById("table");
      var intervalos = document.getElementsByClassName("interval");
      var creados = 0
-     
+     var numFilas = [];
+     var aleatorio = Math.random()*100
+     if(aleatorio > 66 && aleatorio<100){
+          console.log("a")
+     }
+     else if(aleatorio< 66 && aleatorio>33){
+          console.log("b")
+     }
+     else{
+          console.log("no el delegado")
+     }
+
+
+
      intervalos[0].addEventListener("click",function(){
-          crear();
+          /*var ultimo = tr.length -1
+          tr[ultimo].appendChild(
+          */
+          var ultimo = td.length - 1
+          console.log(td[ultimo])
+          numFilas[creados] = new Filas("text",td,ultimo);
+          numFilas[creados] = new Filas("number",td,ultimo)
           creados++;
      })
      intervalos[1].addEventListener("click",function(){
@@ -16,12 +37,31 @@ function agrupados(){
                table.removeChild(borrar);
                creados--;
           }
-
+          
      })
+
+}
+
+class Filas{
+     constructor(tipoInput,td,ultimo){
+          var fila = document.createElement("tr");
+          fila.setAttribute("class","fila");
+          table.appendChild(fila);          
+          
+          var cuadro = document.createElement("td")
+          cuadro.setAttribute("class","td")
+          fila.appendChild(cuadro)
+
+          var input = document.createElement("input")
+          input.setAttribute("type",tipoInput)
+          fila.appendChild(input)
+
+     }
 }
 
 
 
+/*
 function crear(){
      var tr = document.createElement("tr");
      tr.setAttribute("class","fila");
@@ -43,3 +83,4 @@ function crear(){
           }
      }    
 }
+/* */ 
